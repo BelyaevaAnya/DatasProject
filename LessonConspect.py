a8275cfdcea62c87a2617971417f932c90e20628
Anya_Belyaeva <71212278+BelyaevaAnya@users.noreply.github.com>
2024-07-19 19:24:33 +0300

    1 HW

---
+++ LessonConspect.py
@@ -0,0 +1,38 @@
+# int-integer
+print(5)
+print(type(5))
+# sum
+print(5+5)
+print(5/5)  #обычное деление
+print(5//5) #целочисленное деление
+print(5%5)  #остаток
+print(5*5)
+print(5**2) #возведение в степень
+print(2.0)
+print(type(2.0))
+#Изменяемы и неизменяемы типы данных
+print("Hello world")
+print(type("Hello world")) #str-string
+print("Hello '' world") #одинарные
+print('Hello "" world') #двойные
+#concatenate
+print('Hello, '+'world!')
+#единственная операция между строками-это сложение
+#print('1'+11) TypeError str(not 'int')
+print('1'+'1')
+print(1+1)
+#boolean
+print(True, False)
+print(type(True), type(False))
+print(5>10, 5<10)
+print(5<10)
+print(5<10, 1.2, 'Hello, world!')
+print(5!=10 and 5<10)
+print(5!=5 and 5<10)
+#
+print(5!=5 or 5<10)
+print(5!=10 or 5<10)
+print(type(int('5')))
+print(type(float('5')))
+print(type(bool('5')))
+print(bool('5'))
---
+++ task4.py
@@ -0,0 +1,2 @@
+#4th program
+print((int(13.42) == int(round((42.13 % 1)*100))) or (int(round((13.42 % 1)*100)) == int(42.13)))
